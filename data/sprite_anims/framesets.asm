gb-mobile/pokecrystal-mobile-en

Attempt to fix up mobile ezchat menu (#7)
* wram: identify ezchat word vars

* mobile/ezchat: pull ryuzac's latest version

* wram: identify some ezchat stuff

* ezchat: checkpoint 1

- main menu fixed

- changed cursor of bottom menu in alphabet
  category select so as to not overflow the sprite limit

- full word support

* wram: identify more ezchat stuff

* ezchat: sort out word menu and OK autoselect

* ezchat: deal with string clear on word change

* ezchat: checkpoint 2

trying to understand how sorted words work

* ezchat: fix word sorting order

* ezchat: fix navigation of sorted menu

* ezchat: clean up reference scaffolding

data/sprite_anims/framesets.asm
@@ -66,6 +66,8 @@ SpriteAnimFrameData:
 	dw .Frameset_IntroUnownF
 	dw .Frameset_CelebiLeft
 	dw .Frameset_CelebiRight
+	dw .Frameset_EZChatCursor8
+	dw .Frameset_EZChatCursor9
 
 .Frameset_00:
 	frame SPRITE_ANIM_OAMSET_RED_WALK_1, 32
@@ -367,6 +369,14 @@ SpriteAnimFrameData:
 
 .Frameset_EZChatCursor7:
 	frame SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_7,  2
+	endanim
+
+.Frameset_EZChatCursor8:
+	frame SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_8,  2
+	endanim
+
+.Frameset_EZChatCursor9:
+	frame SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_9,  2
 	endanim
 
 .Frameset_MobileTradeSentBall:

data/sprite_anims/oam.asm
@@ -141,6 +141,8 @@ SpriteAnimOAMData:
 	dbw $08, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_9
 	dbw $04, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_10
 	dbw $00, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_11
+	dbw $00, .OAMData_EZChatCursor8            ; SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_8
+	dbw $00, .OAMData_EZChatCursor9            ; SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_9
 
 .OAMData_1x1_Palette0:
 	db 1
@@ -739,6 +741,33 @@ SpriteAnimOAMData:
 	db 2
 	dsprite  0,  0,  0,  0, $32, 0
 	dsprite  1,  0,  0,  0, $35, 0
+
+; wider cursor in the menu screen
+.OAMData_EZChatCursor8:
+	db 18
+	dsprite  0,  0,  0,  0, $30, 0
+	dsprite  0,  0,  1,  0, $31, 0
+	dsprite  0,  0,  2,  0, $31, 0
+	dsprite  0,  0,  3,  0, $31, 0
+	dsprite  0,  0,  4,  0, $31, 0
+	dsprite  0,  0,  5,  0, $31, 0
+	dsprite  0,  0,  6,  0, $31, 0
+	dsprite  0,  0,  7,  0, $31, 0
+	dsprite  0,  0,  8,  0, $32, 0
+	dsprite  1,  0,  0,  0, $33, 0
+	dsprite  1,  0,  1,  0, $34, 0
+	dsprite  1,  0,  2,  0, $34, 0
+	dsprite  1,  0,  3,  0, $34, 0
+	dsprite  1,  0,  4,  0, $34, 0
+	dsprite  1,  0,  5,  0, $34, 0
+	dsprite  1,  0,  6,  0, $34, 0
+	dsprite  1,  0,  7,  0, $34, 0
+	dsprite  1,  0,  8,  0, $35, 0
+
+; arrow substitute
+.OAMData_EZChatCursor9:
+	db 1
+	dsprite  1,  -2,  -1,  4, $ed, 0
 
 .OAMData_BlueWalk:
 	db 4

xaerochill/liquid-crystal

data/sprite_anims/framesets.asm
@@ -67,6 +67,8 @@ SpriteAnimFrameData:
 	dw .Frameset_IntroUnownF
 	dw .Frameset_CelebiLeft
 	dw .Frameset_CelebiRight
+	dw .Frameset_EZChatCursor8
+	dw .Frameset_EZChatCursor9
 	assert_table_length NUM_SPRITE_ANIM_FRAMESETS
 
 .Frameset_00:
@@ -369,6 +371,14 @@ SpriteAnimFrameData:
 
 .Frameset_EZChatCursor7:
 	oamframe SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_7,  2
+	oamend
+
+.Frameset_EZChatCursor8:
+	oamframe SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_8,  2
+	oamend
+
+.Frameset_EZChatCursor9:
+	oamframe SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_9,  2
 	oamend
 
 .Frameset_MobileTradeSentBall:

data/sprite_anims/oam.asm
@@ -147,6 +147,8 @@ SpriteAnimOAMData:
 	spriteanimoam $08, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_9
 	spriteanimoam $04, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_10
 	spriteanimoam $00, .OAMData_GameFreakLogo4_11        ; SPRITE_ANIM_OAMSET_GAMEFREAK_LOGO_11
+	spriteanimoam $00, .OAMData_EZChatCursor8            ; SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_8
+	spriteanimoam $00, .OAMData_EZChatCursor9            ; SPRITE_ANIM_OAMSET_EZCHAT_CURSOR_9
 	assert_table_length NUM_SPRITE_ANIM_OAMSETS
 
 .OAMData_1x1_Palette0:
@@ -746,6 +748,33 @@ SpriteAnimOAMData:
 	db 2
 	dbsprite  0,  0,  0,  0, $32, 0
 	dbsprite  0,  1,  0,  0, $35, 0
+
+; wider cursor in the menu screen
+.OAMData_EZChatCursor8:
+	db 18
+	dsprite  0,  0,  0,  0, $30, 0
+	dsprite  0,  0,  1,  0, $31, 0
+	dsprite  0,  0,  2,  0, $31, 0
+	dsprite  0,  0,  3,  0, $31, 0
+	dsprite  0,  0,  4,  0, $31, 0
+	dsprite  0,  0,  5,  0, $31, 0
+	dsprite  0,  0,  6,  0, $31, 0
+	dsprite  0,  0,  7,  0, $31, 0
+	dsprite  0,  0,  8,  0, $32, 0
+	dsprite  1,  0,  0,  0, $33, 0
+	dsprite  1,  0,  1,  0, $34, 0
+	dsprite  1,  0,  2,  0, $34, 0
+	dsprite  1,  0,  3,  0, $34, 0
+	dsprite  1,  0,  4,  0, $34, 0
+	dsprite  1,  0,  5,  0, $34, 0
+	dsprite  1,  0,  6,  0, $34, 0
+	dsprite  1,  0,  7,  0, $34, 0
+	dsprite  1,  0,  8,  0, $35, 0
+
+; arrow substitute
+.OAMData_EZChatCursor9:
+	db 1
+	dsprite  1,  -2,  -1,  4, $ed, 0
 
 .OAMData_BlueWalk:
 	db 4