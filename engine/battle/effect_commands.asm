12be2957fa578f09e30441184b2b55dc02879266
xaerochill <enosnemeis@gmail.com>
2023-03-14 09:24:11 +0100

    Fix Reflect and Light Screen Bug

--- engine/battle/effect_commands.asm
+++ engine/battle/effect_commands.asm
@@ -2642,10 +2642,6 @@ TruncateHL_BC:
 	inc l
 
 .finish
-; BUG: Reflect and Light Screen can make (Special) Defense wrap around above 1024 (see docs/bugs_and_glitches.md)
-	ld a, [wLinkMode]
-	cp LINK_COLOSSEUM
-	jr z, .done
 ; If we go back to the loop point,
 ; it's the same as doing this exact
 ; same check twice.
