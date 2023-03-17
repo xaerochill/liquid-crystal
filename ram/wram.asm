2a7de08033b58d4dc142444b10de714f1444ab75
xaerochill <enosnemeis@gmail.com>
2023-03-17 12:15:56 +0100

    Reduce the command queue system to just stone tables

--- ram/wram.asm
+++ ram/wram.asm
@@ -2987,9 +2987,9 @@ for n, 1, NUM_OBJECT_STRUCTS
 wObject{d:n}Struct:: object_struct wObject{d:n}
 endr
 
-wCmdQueue:: ds CMDQUEUE_CAPACITY * CMDQUEUE_ENTRY_SIZE
+wStoneTableAddress:: dw
 
-	ds 6
+	ds 28
 
 wMapObjects::
 wPlayerObject:: map_object wPlayer ; player is map object 0
